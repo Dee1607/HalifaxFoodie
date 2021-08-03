f7541ab14b5d23b601d7384fd4b4e567f5dd150c
Dhruv Doshi <dh722257@dal.ca>
2021-08-02 22:19:20 -0300

    Delete index.js

--- pubsubapi/pubsubapi/index.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const port = process.env.PORT || 5001;
-const app = require('./app');
-const server = http.createServer(app);
-server.listen(port, () => {
-    console.log("satrting server at http://localhost:"+port);
-})
